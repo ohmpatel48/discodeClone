58659b5687023d28bc1486fa7855efcd12dad2d8
ohmpatel48 <ohm.pce21@sot.pdpu.ac.in>
2024-08-04 23:44:59 +0530

    models completed

--- mainpage/src/main/resources/static/js/change.js
+++ mainpage/src/main/resources/static/js/change.js
@@ -13,3 +13,23 @@ function changetox() {
     toggle = "arrow";
   }
 }
+function previewImage(event) {
+  const [file] = event.target.files;
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const imagePreview = document.getElementById('avatar_container');
+      const uploadIcon = document.getElementById('uploadIcon');
+      document.getElementById("imagePreview").src = e.target.result;
+      imagePreview.classList.remove('hidden');
+      uploadIcon.classList.add('hidden');
+    };
+    reader.readAsDataURL(file);
+  }
+}
+document.getElementById('remove_avatar').addEventListener('click', function () {
+  const avatar = document.getElementById('avatar');
+  avatar.src = '';
+  document.getElementById('avatar_container').classList.add('hidden');
+  document.getElementById('image_input').classList.remove('hidden');
+});
